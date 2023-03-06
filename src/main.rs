c148e255fb000d75036d34091ad380181afd80b4
alexxmatei <alexxmatei@gmail.com>
2023-03-06 08:37:20 +0200

    Add rand crate + test code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    let mut rng = rand::thread_rng();
+    let random_number = rng.gen_range(1..=10);
+    println!("Random number between 1 and 10: {}", random_number);
 }
