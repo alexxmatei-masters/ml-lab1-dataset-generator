3d1cc34fbd7d8f606e0fe257c6d8a4c04a1c2103
alexxmatei <alexxmatei@gmail.com>
2023-03-08 13:47:29 +0200

    Add explicit types

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
+use rand::rngs::ThreadRng;
 use rand::Rng;
 
 fn main() {
-    let mut rng = rand::thread_rng();
-    let random_number = rng.gen_range(1..=10);
+    let mut rng: ThreadRng = rand::thread_rng();
+    let random_number: i16 = rng.gen_range(1..=10);
     println!("Random number between 1 and 10: {}", random_number);
 }
